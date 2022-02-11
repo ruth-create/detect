4f2d9366c947028a49b22f9390eeb02db3875ba8
ruth-create <80636279+ruth-create@users.noreply.github.com>
2022-02-11 18:48:23 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+img="";
+status="";
+objects=[];
+
+function setup(){
+    canvas=createCanvas(400,400);
+    //canvas.center();
+    document.getElementById("status").innerHTML="Defining Images";
+    objectDetector=ml5.objectDetector("cocossd", modelLoaded);
+}
+
+function preload(){
+    img=loadImage("bottle.jpeg");
+
+}
+
+function draw(){
+    image(img,0,0,400,400);
+    if(status !=""){
+    for(i=0; i< objects.length; i++){
+    document.getElementById("status").innerHTML="Status: Objects Detected";
+    fill("#8c035a");
+    percent=ceil(objects[i].confidence*100);
+    text(objects[i].label+""+percent+"%",objects[i].x+40,objects[i].y+40);
+    noFill();
+    stroke("#8c035a");
+    rect(objects[i].x, objects[i].y,objects[i].width, objects[i].height);
+    }
+}}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+objects=results;
+    }
+}
